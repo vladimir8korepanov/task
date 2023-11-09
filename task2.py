207aabdac40b89d0615df3f8ac42f505e7e0e6aa
vladimir8korepanov <149078785+vladimir8korepanov@users.noreply.github.com>
2023-11-09 16:03:15 +0400

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,46 +1,41 @@
 import sys
-import math
 
-def read_coordinates(text):
-    coordinates = []
-    lines = text.split('\n')
-    for line in lines:
-        if line:
-            x, y = map(float, line.strip().split())
-            coordinates.append((x, y))
-    return coordinates
 
-def calculate_distance(x1, y1, x2, y2):
-    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+def read_circle_data(file_path):
+    with open(file_path, 'r') as file:
+        x, y, r = map(float, file.readline().split())
+    return x, y, r
 
-def check_point_position(center_x, center_y, radius, point_x, point_y):
-    distance = calculate_distance(center_x, center_y, point_x, point_y)
-    if math.isclose(distance, radius):
-        return 0  # точка лежит на окружности
-    elif distance < radius:
+
+def read_points(file_path):
+    with open(file_path, 'r') as file:
+        points = []
+        for line in file:
+            x, y = map(float, line.split())
+            points.append((x, y))
+    return points
+
+
+def point_position(circle_x, circle_y, radius, point_x, point_y):
+    distance_square = (point_x - circle_x) ** 2 + (point_y - circle_y) ** 2
+    if distance_square < radius ** 2:
         return 1  # точка внутри окружности
-    else:
+    elif distance_square > radius ** 2:
         return 2  # точка снаружи окружности
+    else:
+        return 0  # точка на окружности
 
-# Проверяем, что переданы два аргумента - имена файлов
-if len(sys.argv) != 3:
-    print("Необходимо передать два аргумента: файл с координатами и радиусом окружности, и файл с координатами точек.")
-    sys.exit(1)
 
-# Считываем содержимое файлов
-with open(sys.argv[1], 'r') as file:
-    circle_text = file.read()
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python task2.py <circle_file_path> <points_file_path>")
+    else:
+        circle_file_path = sys.argv[1]
+        points_file_path = sys.argv[2]
 
-with open(sys.argv[2], 'r') as file:
-    points_text = file.read()
+        circle_x, circle_y, radius = read_circle_data(circle_file_path)
+        points = read_points(points_file_path)
 
-# Считываем координаты и радиус окружности из файла
-circle_coordinates = read_coordinates(circle_text)
-center_x, center_y, radius = circle_coordinates[0]
-
-# Считываем координаты точек из файла и проверяем их положение относительно окружности
-point_coordinates = read_coordinates(points_text)
-
-for point_x, point_y in point_coordinates:
-    position = check_point_position(center_x, center_y, radius, point_x, point_y)
-    print(position)
+        for point in points:
+            result = point_position(circle_x, circle_y, radius, point[0], point[1])
+            print(result)
